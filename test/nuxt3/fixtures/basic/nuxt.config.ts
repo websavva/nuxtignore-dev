4576df52d1b904fee7c3547144c7b06a7cd818db
WebSavva <websavvap@gmail.com>
2024-10-10 01:09:19 +0300

    fix: aliases are handled when pages:extend hook is processed

--- test/nuxt3/fixtures/basic/nuxt.config.ts
+++ test/nuxt3/fixtures/basic/nuxt.config.ts
@@ -1,5 +1,3 @@
-import { createResolver } from '@nuxt/kit';
-
 import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
@@ -9,23 +7,22 @@ export default defineNuxtConfig({
 
   hooks: {
     'pages:extend'(pages) {
-      const { resolve } = createResolver(import.meta.url);
       pages.push(
         {
           name: 'ActiveExtendedPage',
-          file: resolve('./extended-pages/ActivePage.vue'),
+          file: '~/extended-pages/ActivePage.vue',
           path: '/extended/active',
           children: [
             {
               name: 'IgnoreChildExtendedPage',
               path: '/extended/active/child',
-              file: resolve('./extended-pages/IgnoredChildPage.vue'),
+              file: '~/extended-pages/IgnoredChildPage.vue',
             },
           ],
         },
         {
           name: 'IgnoredExtendedPage',
-          file: resolve('./extended-pages/IgnoredPage.vue'),
+          file: '~/extended-pages/IgnoredPage.vue',
           path: '/extended/ignored',
         },
       );
